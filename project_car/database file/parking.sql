eeb8834a4a4cefb4623e31fdd17c8183313e4e46
Md. Shariar Emon Shaikat <85901960+shariar26868@users.noreply.github.com>
2023-12-11 22:53:11 +0600

    Update parking.sql

--- project_car/database file/parking.sql
+++ project_car/database file/parking.sql
@@ -1,8 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 11, 2023 at 05:45 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `parking`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking`
+--
 
 CREATE TABLE `booking` (
   `booking_id` int(11) NOT NULL,
@@ -10,10 +33,23 @@ CREATE TABLE `booking` (
   `car_id` int(11) DEFAULT NULL,
   `parking_lot_id` int(11) DEFAULT NULL,
   `start_time` datetime DEFAULT NULL,
-  `end_time` datetime DEFAULT NULL
+  `end_time` datetime DEFAULT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `booking`
+--
 
+INSERT INTO `booking` (`booking_id`, `assigned_driver_id`, `car_id`, `parking_lot_id`, `start_time`, `end_time`, `time`) VALUES
+(120, 8, 163, 11, '2023-12-11 22:11:00', '2023-12-11 23:11:00', '2023-12-11 16:18:56'),
+(121, 8, 163, 11, '2023-12-11 22:21:00', '2023-12-11 23:21:00', '2023-12-11 16:21:37');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `car`
+--
 
 CREATE TABLE `car` (
   `car_id` int(11) NOT NULL,
@@ -23,6 +59,18 @@ CREATE TABLE `car` (
   `driver_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `car`
+--
+
+INSERT INTO `car` (`car_id`, `license_plate_number`, `model`, `registration_year`, `driver_id`) VALUES
+(163, 'GA2069', 'Toyota Premio', 2012, 8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `driver`
+--
 
 CREATE TABLE `driver` (
   `driver_id` int(11) NOT NULL,
@@ -33,11 +81,18 @@ CREATE TABLE `driver` (
   `password` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `driver`
+--
 
 INSERT INTO `driver` (`driver_id`, `name`, `email`, `phone_number`, `license_number`, `password`) VALUES
-(6, 'Tazower Sowad ', 'tazower000sowad@gmail.com', '01580719790', 'GA2069', 'asdf');
+(8, 'Tazower', 'tazower000sowad@gmail.com', '01580719791', '753421869', 'asdf');
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `parkinglot`
+--
 
 CREATE TABLE `parkinglot` (
   `parking_lot_id` int(11) NOT NULL,
@@ -47,6 +102,19 @@ CREATE TABLE `parkinglot` (
   `parking_owner_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `parkinglot`
+--
+
+INSERT INTO `parkinglot` (`parking_lot_id`, `address`, `capacity`, `space_availability`, `parking_owner_id`) VALUES
+(11, '12/C, St. Bakers, London', 6, 5, 4),
+(12, '161/2/B, Dhaka cantonment, Dhaka', 5, 5, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parkingowner`
+--
 
 CREATE TABLE `parkingowner` (
   `parking_owner_id` int(11) NOT NULL,
@@ -57,6 +125,18 @@ CREATE TABLE `parkingowner` (
   `password` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `parkingowner`
+--
+
+INSERT INTO `parkingowner` (`parking_owner_id`, `name`, `email`, `phone_number`, `address`, `password`) VALUES
+(4, 'John Doe', 'johndoe60@gmail.com', '01580719791', 'Bakers St, London', 'asdf');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
 
 CREATE TABLE `payment` (
   `payment_id` int(11) NOT NULL,
@@ -66,6 +146,19 @@ CREATE TABLE `payment` (
   `payment_date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `payment`
+--
+
+INSERT INTO `payment` (`payment_id`, `booking_id`, `payment_method`, `payment_amount`, `payment_date`) VALUES
+(54, 120, 'cash', 50.00, '2023-12-11'),
+(55, 121, 'cash', 50.00, '2023-12-11');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `review`
+--
 
 CREATE TABLE `review` (
   `review_id` int(11) NOT NULL,
@@ -76,90 +169,151 @@ CREATE TABLE `review` (
   `date_submitted` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `review`
+--
 
+INSERT INTO `review` (`review_id`, `driver_id`, `parking_lot_id`, `rating`, `review_text`, `date_submitted`) VALUES
+(5, 8, 11, 3, "The parking spot wasn\'t properly straight. The place had holes in the ground. ", '2023-12-11');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `booking`
+--
 ALTER TABLE `booking`
   ADD PRIMARY KEY (`booking_id`),
   ADD KEY `assigned_driver_id` (`assigned_driver_id`),
   ADD KEY `car_id` (`car_id`),
   ADD KEY `parking_lot_id` (`parking_lot_id`);
 
-
+--
+-- Indexes for table `car`
+--
 ALTER TABLE `car`
   ADD PRIMARY KEY (`car_id`),
   ADD KEY `car_ibfk_1` (`driver_id`);
 
-
+--
+-- Indexes for table `driver`
+--
 ALTER TABLE `driver`
   ADD PRIMARY KEY (`driver_id`);
 
-
+--
+-- Indexes for table `parkinglot`
+--
 ALTER TABLE `parkinglot`
   ADD PRIMARY KEY (`parking_lot_id`),
   ADD KEY `parking_owner_id` (`parking_owner_id`);
 
-
+--
+-- Indexes for table `parkingowner`
+--
 ALTER TABLE `parkingowner`
   ADD PRIMARY KEY (`parking_owner_id`);
 
-
+--
+-- Indexes for table `payment`
+--
 ALTER TABLE `payment`
   ADD PRIMARY KEY (`payment_id`),
   ADD KEY `booking_id` (`booking_id`);
 
-
+--
+-- Indexes for table `review`
+--
 ALTER TABLE `review`
   ADD PRIMARY KEY (`review_id`),
   ADD KEY `driver_id` (`driver_id`),
   ADD KEY `parking_lot_id` (`parking_lot_id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
+--
+-- AUTO_INCREMENT for table `booking`
+--
 ALTER TABLE `booking`
-  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=122;
 
-
+--
+-- AUTO_INCREMENT for table `car`
+--
 ALTER TABLE `car`
-  MODIFY `car_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `car_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=164;
 
-
+--
+-- AUTO_INCREMENT for table `driver`
+--
 ALTER TABLE `driver`
-  MODIFY `driver_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `driver_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
-
+--
+-- AUTO_INCREMENT for table `parkinglot`
+--
 ALTER TABLE `parkinglot`
-  MODIFY `parking_lot_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `parking_lot_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
-
+--
+-- AUTO_INCREMENT for table `parkingowner`
+--
 ALTER TABLE `parkingowner`
-  MODIFY `parking_owner_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `parking_owner_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
-
+--
+-- AUTO_INCREMENT for table `payment`
+--
 ALTER TABLE `payment`
-  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 
-
+--
+-- AUTO_INCREMENT for table `review`
+--
 ALTER TABLE `review`
-  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
+--
+-- Constraints for dumped tables
+--
 
+--
+-- Constraints for table `booking`
+--
 ALTER TABLE `booking`
   ADD CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`assigned_driver_id`) REFERENCES `driver` (`driver_id`) ON DELETE CASCADE,
   ADD CONSTRAINT `booking_ibfk_2` FOREIGN KEY (`car_id`) REFERENCES `car` (`car_id`) ON DELETE CASCADE,
   ADD CONSTRAINT `booking_ibfk_3` FOREIGN KEY (`parking_lot_id`) REFERENCES `parkinglot` (`parking_lot_id`) ON DELETE CASCADE;
 
+--
+-- Constraints for table `car`
+--
 ALTER TABLE `car`
   ADD CONSTRAINT `car_ibfk_1` FOREIGN KEY (`driver_id`) REFERENCES `driver` (`driver_id`) ON DELETE SET NULL;
 
-
+--
+-- Constraints for table `parkinglot`
+--
 ALTER TABLE `parkinglot`
   ADD CONSTRAINT `parkinglot_ibfk_1` FOREIGN KEY (`parking_owner_id`) REFERENCES `parkingowner` (`parking_owner_id`) ON DELETE CASCADE;
 
-
+--
+-- Constraints for table `payment`
+--
 ALTER TABLE `payment`
   ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`booking_id`) REFERENCES `booking` (`booking_id`) ON DELETE CASCADE;
 
-
+--
+-- Constraints for table `review`
+--
 ALTER TABLE `review`
   ADD CONSTRAINT `review_ibfk_1` FOREIGN KEY (`driver_id`) REFERENCES `driver` (`driver_id`) ON DELETE CASCADE,
   ADD CONSTRAINT `review_ibfk_2` FOREIGN KEY (`parking_lot_id`) REFERENCES `parkinglot` (`parking_lot_id`) ON DELETE CASCADE;
 COMMIT;
 
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
